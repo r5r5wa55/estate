d087cc09ca8fb19a142ae2ef716cecf355de1acc
werapol <r5r5wa55@hotmail.com>
2024-03-17 15:20:22 +0700

    commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Header from "./assets/components/Header"
+
+
+
+function App() {
+
+
+  return (
+    <>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
