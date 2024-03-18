bbbbcdffcca14730e7d2811087a2e6ce5f145064
werapol <r5r5wa55@hotmail.com>
2024-03-18 19:17:04 +0700

    create api sign-in and jwt

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from '../models/user.model.js';
 import bcryptjs from 'bcryptjs';
 import { errorHandler} from '../utils/error.js';
-
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -9,9 +9,26 @@ export const signup = async (req, res, next) => {
   const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
-    res.status(201).json('User created successfully!');
+    res.status(201).json('สร้างชื่อผู้ใช้นี้เรียบร้อย');
   } catch (error) {
     next(error);
   }
 };
+
+export const signin=async(req,res,next)=>{
+  const {email,password}=req.body;
+  try {
+    const vaildUser=await User.findOne({email})
+    if(!vaildUser) return next(errorHandler(404,'ไม่มีข้อมูลผู้ใช้'))
+    const vaildPassword=await bcryptjs.compareSync(password,vaildUser.password)
+    if(!vaildPassword) return next(errorHandler(401,'ตรวจสอบรหัสผ่าน'))
+    const token = jwt.sign({id:vaildUser._id},process.env.JWT_SECRET)
+    const {password:pass,...we} = vaildUser;
+    res.cookie('access_token',token,{httpOnly:true})
+    .status(200)
+    .json(we)
+  } catch (error) {
+    next(error);
+  }
+}
 
