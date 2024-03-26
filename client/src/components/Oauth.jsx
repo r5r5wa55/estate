a6652856757352454baf9de7785f58bb342dfc45
werapol <r5r5wa55@hotmail.com>
2024-03-26 14:51:35 +0700

    auth google

--- client/src/components/Oauth.jsx
+++ client/src/components/Oauth.jsx
@@ -1,10 +1,31 @@
 
-
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { app } from "../firebase";
+import {useDispatch} from 'react-redux'
+import {signInSuccess} from '../redux/user/userSlice.js'
+import {useNavigate} from 'react-router-dom'
 export default function Oauth() {
-
+  const dispath = useDispatch()
+  const navigate = useNavigate()
+  const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
+  console.log(generatedPassword);
   const handleSubmit =async()=>{
     try {
-      console.log('we');
+      const provider = new GoogleAuthProvider()
+      const auth = getAuth(app)
+      const result = await signInWithPopup(auth,provider)
+      const res = await fetch(`/api/auth/google`,{
+        method:'POST',
+        headers:{
+          'Content-Type':'applocation',
+        },
+        body:JSON.stringify({name:result.user.displayName,email:result.user.email,photo:result.user.photoURL})
+      })
+
+      const data = await res.json()
+      dispath(signInSuccess(data))
+      navigate('/')
+      console.log(res);
     } catch (error) {
       console.log(`could not sign in with google`);
     }
@@ -16,7 +37,7 @@ export default function Oauth() {
       type="button"
       onClick={handleSubmit}
       className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 text-center'>
-      Login with Google
+        Login with Google
     </button>
   )
 }
