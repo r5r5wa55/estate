058e120ef30c34802f061ffc97945740bcc02f0f
werapol <r5r5wa55@hotmail.com>
2024-03-31 14:02:34 +0700

    fix bug component req /json

--- client/src/components/Oauth.jsx
+++ client/src/components/Oauth.jsx
@@ -1,43 +1,46 @@
-
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 import { app } from "../firebase";
-import {useDispatch} from 'react-redux'
-import {signInSuccess} from '../redux/user/userSlice.js'
-import {useNavigate} from 'react-router-dom'
+import { useDispatch } from "react-redux";
+import { signInSuccess } from "../redux/user/userSlice.js";
+import { useNavigate } from "react-router-dom";
 export default function Oauth() {
-  const dispath = useDispatch()
-  const navigate = useNavigate()
-  const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
+  const dispath = useDispatch();
+  const navigate = useNavigate();
+  const generatedPassword =
+    Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
   console.log(generatedPassword);
-  const handleSubmit =async()=>{
+  const handleSubmit = async () => {
     try {
-      const provider = new GoogleAuthProvider()
-      const auth = getAuth(app)
-      const result = await signInWithPopup(auth,provider)
-      const res = await fetch(`/api/auth/google`,{
-        method:'POST',
-        headers:{
-          'Content-Type':'applocation',
+      const provider = new GoogleAuthProvider();
+      const auth = getAuth(app);
+      const result = await signInWithPopup(auth, provider);
+      const res = await fetch(`/api/auth/google`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
         },
-        body:JSON.stringify({name:result.user.displayName,email:result.user.email,photo:result.user.photoURL})
-      })
+        body: JSON.stringify({
+          name: result.user.displayName,
+          email: result.user.email,
+          photo: result.user.photoURL,
+        }),
+      });
 
-      const data = await res.json()
-      dispath(signInSuccess(data))
-      navigate('/')
-      console.log(res);
+      const data = await res.json();
+      dispath(signInSuccess(data));
+      navigate("/");
     } catch (error) {
       console.log(`could not sign in with google`);
     }
-
-  }
+  };
 
   return (
-    <button 
+    <button
       type="button"
       onClick={handleSubmit}
-      className='bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 text-center'>
-        Login with Google
+      className="bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80 text-center"
+    >
+      Login with Google
     </button>
-  )
+  );
 }
